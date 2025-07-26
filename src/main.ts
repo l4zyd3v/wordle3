2c5d4d9b18dc7be506a815c9e1ae8e7c7518cc29
l4zyd3v <nielsen.177@gmail.com>
2025-07-26 08:20:14 +0200

    pong

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
 import "./style.css";
+
+const createElement = (element: string, className?: string): HTMLElement => {
+  const elm = document.createElement(element) as HTMLElement;
+  className && elm.classList.add(className);
+
+  return elm;
+};
+
+const createGame = () => {
+  const app = document.querySelector<HTMLDivElement>("#app")!;
+  const gameBoard = createElement("div", "gameBoard");
+  const player1 = createElement("div", "player1");
+  const player2 = createElement("div", "player2");
+  const ball = createElement("div", "ball");
+
+  app.appendChild(gameBoard);
+  gameBoard.appendChild(player1);
+  gameBoard.appendChild(player2);
+  gameBoard.appendChild(ball);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  createGame();
+});
+
+const startGame = () => {};
