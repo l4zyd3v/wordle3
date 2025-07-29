97e23102ff5d3815cb95f3aaba01e08c11116907
l4zyd3v <nielsen.177@gmail.com>
2025-07-29 10:22:12 +0200

    changes

--- src/main.ts
+++ src/main.ts
@@ -7,31 +7,37 @@ const createElement = (element: string, className?: string): HTMLElement => {
   return elm;
 };
 
-const createGame = () => {
+const createNewGame = () => {
   const app = document.querySelector<HTMLDivElement>("#app")!;
   const gameBoard = createElement("div", "gameBoard");
   const player1 = createElement("div", "player1");
   const player2 = createElement("div", "player2");
   const ball = createElement("div", "ball");
+  const button = createElement("button", "startButton");
+  button.textContent = "Start Game";
+  button.style.position = "absolute";
+  button.style.top = "10px";
 
   app.appendChild(gameBoard);
+  app.appendChild(button);
   gameBoard.appendChild(player1);
   gameBoard.appendChild(player2);
   gameBoard.appendChild(ball);
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  createGame();
-  control();
-});
-
-const startGame = () => {};
-
-const control = () => {
+const initiateControl = () => {
   const player1 = document.querySelector(".player1");
-  console.log(player1);
+  const player2 = document.querySelector(".player2");
+  const startBtn = document.querySelector(".startButton");
 
-  player1?.addEventListener("click", () => {
-    console.log("testtest");
-  });
+  startBtn?.addEventListener("click", () => {});
+
+  document.addEventListener("keydown", (event) => {});
 };
+
+const start = () => {
+  createNewGame();
+  initiateControl();
+};
+
+document.addEventListener("DOMContentLoaded", start);
